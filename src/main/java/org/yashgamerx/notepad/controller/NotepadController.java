a7054669c23e69f863c31c9bb6242b1be14db4b1
Yashgamer-x <yashdesai121212@gmail.com>
2025-11-19 18:37:12 -0500

    NotepadController::onOpenFile requires a fixation because the parent of the action is not TextArea bu

--- src/main/java/org/yashgamerx/notepad/controller/NotepadController.java
+++ src/main/java/org/yashgamerx/notepad/controller/NotepadController.java
@@ -1,8 +1,14 @@
 package org.yashgamerx.notepad.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
 import org.yashgamerx.notepad.handler.StageHandler;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class NotepadController {
 
@@ -13,5 +19,26 @@ public class NotepadController {
     private void initialize() {
         var stage = StageHandler.getStage();
         stage.opacityProperty().bind(scaleSlider.valueProperty().divide(100.0));
+    }
+
+    @FXML
+    private void onOpenFile(ActionEvent actionEvent) {
+        //Step 1: Initialization
+        var textArea = (TextArea) actionEvent.getSource(); //TODO: Change the action.getSource to textArea id
+        var fileChooser = new FileChooser();
+
+        //Step 2: Setup File Chooser
+        fileChooser.setTitle("Open Text File");
+
+        //Step 3: Opens the dialog for selecting file
+        var file = fileChooser.showOpenDialog(StageHandler.getStage());
+        if (file == null) return;
+
+        try {
+            String content = Files.readString(file.toPath());
+            textArea.setText(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
