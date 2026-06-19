3e3db9395be4149e7da867a03281070163fce0c9
Yashgamer-x <yashdesai121212@gmail.com>
2026-06-18 23:38:24 -0400

    Refactor NotepadTabFactory to NotepadTabGenerator, introduce TabGenerator interface and TabContext for cleaner dependency injection. Update module exports accordingly.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -23,7 +23,7 @@ module org.yashgamerx.notepad {
     opens org.yashgamerx.notepad.view to javafx.fxml, javafx.graphics;
 
     // Open packages to Spring for dependency injection & bean creation
-    opens org.yashgamerx.notepad.factory to spring.beans, spring.core, spring.context;
+    opens org.yashgamerx.notepad.generator to spring.beans, spring.core, spring.context;
     opens org.yashgamerx.notepad.handler to spring.beans, spring.core, spring.context, javafx.fxml;
     opens org.yashgamerx.notepad.service to spring.beans, spring.core, spring.context, javafx.fxml;
     opens org.yashgamerx.notepad.viewmodel to spring.beans, spring.core, spring.context, javafx.fxml;
@@ -34,11 +34,11 @@ module org.yashgamerx.notepad {
 
     // Export API packages
     exports org.yashgamerx.notepad;
-    exports org.yashgamerx.notepad.factory;
     exports org.yashgamerx.notepad.view;
     exports org.yashgamerx.notepad.handler;
     exports org.yashgamerx.notepad.model;
     exports org.yashgamerx.notepad.viewmodel;
     exports org.yashgamerx.notepad.service;
     exports org.yashgamerx.notepad.settings;
+    exports org.yashgamerx.notepad.generator;
 }
--- src/main/java/org/yashgamerx/notepad/generator/NotepadTabGenerator.java
+++ src/main/java/org/yashgamerx/notepad/generator/NotepadTabGenerator.java
@@ -1,4 +1,4 @@
-package org.yashgamerx.notepad.factory;
+package org.yashgamerx.notepad.generator;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
@@ -6,6 +6,7 @@ import javafx.scene.text.Font;
 import org.springframework.stereotype.Component;
 import org.yashgamerx.notepad.handler.TabNumberHandler;
 import org.yashgamerx.notepad.model.NotepadTabModel;
+import org.yashgamerx.notepad.model.TabContext;
 import org.yashgamerx.notepad.service.FileService;
 import org.yashgamerx.notepad.view.NotepadTabView;
 import org.yashgamerx.notepad.viewmodel.NotepadTabViewModel;
@@ -14,28 +15,31 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 @Component
-public class NotepadTabFactory {
+public class NotepadTabGenerator implements TabGenerator {
     private final FileService fileService;
     private final TabNumberHandler tabNumberHandler;
 
-    public NotepadTabFactory(FileService fileService, TabNumberHandler tabNumberHandler) {
+    public NotepadTabGenerator(FileService fileService, TabNumberHandler tabNumberHandler) {
         this.fileService = fileService;
         this.tabNumberHandler = tabNumberHandler;
     }
 
-    public NotepadTabView create(
-            Path filePath,
-            BooleanProperty wordWrapProperty,
-            ObjectProperty<Font> fontProperty
-    ) throws IOException {
+    @Override
+    public NotepadTabView generate(TabContext context) throws IOException {
+        var filePath = context.filePath();
+        var wordWrapProperty = context.wordWrap();
+        var fontProperty = context.font();
+
         String title = (filePath == null)
                 ? "Untitled " + tabNumberHandler.postIncrement()
                 : filePath.getFileName().toString();
+
         var model = new NotepadTabModel(title, filePath);
         var vm = new NotepadTabViewModel(model, fileService);
         var view = new NotepadTabView();
         view.bind(vm, wordWrapProperty, fontProperty);
         vm.load();
+
         return view;
     }
 }
